9d02afd1a51f700c523db203bc63de2bad76207f
chenqw <gdnpc@163.com>
2024-11-14 15:16:59 +0800

    -[修改]补充全局属性是否支持复选的校验
    Signed-off-by: chenqw <gdnpc@163.com>

--- src/main/java/neatlogic/framework/cmdb/exception/globalattr/GlobalAttrValueIrregularException.java
+++ src/main/java/neatlogic/framework/cmdb/exception/globalattr/GlobalAttrValueIrregularException.java
@@ -12,4 +12,10 @@ public class GlobalAttrValueIrregularException extends ApiRuntimeException {
     public GlobalAttrValueIrregularException(String ciLabel, String ciName, GlobalAttrVo globalAttrVo, String value, String configurationPath, String actualPath) {
         super("nfceg.globalattrvalueirregularexception.globalattrvalueirregularexception_b", ciLabel, ciName, globalAttrVo.getLabel(), globalAttrVo.getName(), value, configurationPath, actualPath);
     }
+
+    static public class MultipleException extends ApiRuntimeException {
+        public MultipleException(GlobalAttrVo globalAttrVo) {
+            super("全局属性“" + globalAttrVo.getLabel() + "”不支持多选");
+        }
+    }
 }
